97d60892fd4c72d4bdd7b39a11e3a736b0d6d199
Ali ABDELSALAM <ali.abdelsalam@tuni.fi>
2023-02-15 22:27:45 +0200

    '15/12'

--- src/components/Chat/ChatMessage/EmojiBubble.vue
+++ src/components/Chat/ChatMessage/EmojiBubble.vue
@@ -14,34 +14,19 @@ const { cssVars } = useChatBubblePosition(props.direction);
 </script>
 
 <template>
-  <div class="chat-bubble shadow-1" :style="cssVars">
+  <div :style="cssVars">
     <slot />
   </div>
 </template>
 
 <style scoped lang="scss">
-.chat-bubble {
-  color: #281822;
-  border-radius: 10px;
-  padding: 0.5rem 1rem;
+img {
+  width: 48px;
+  height: 48px;
   display: inline-block;
   position: relative;
   align-self: var(--align);
   margin: var(--margin);
-
-  &::after {
-    content: "";
-    display: block;
-    position: absolute;
-    top: 0;
-    left: var(--left);
-    right: var(--right);
-    height: 20px;
-    width: 20px;
-    background-color: inherit;
-    clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 5%);
-    border-radius: 0.25rem 0 0 0;
-    transform: var(--transform);
-  }
 }
+
 </style>
